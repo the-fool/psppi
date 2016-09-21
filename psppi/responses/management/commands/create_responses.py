eef14d33b239895e8400f74a6b38a65e1512d788
the-fool <sketchbang@gmail.com>
2016-09-20 22:49:24 -0500

    rethinking the models

--- psppi/responses/management/commands/create_responses.py
+++ psppi/responses/management/commands/create_responses.py
@@ -3,27 +3,45 @@ from psppi.questions.models import Question
 from psppi.responses.models import Response, Demography
 import pandas as pd
 import os
+from collections import defaultdict
 
 class Command(BaseCommand):
-    help = "Pulls question information"
+    help = "Gets all the responses!"
 
     def handle(self, *args, **kwargs):
-        Question.objects.all().delete()
-        questions = []
-        with open('./data/questions.csv') as f:
-            df = pd.read_csv(f, header=0)
-            for _, row in df.iterrows():
-                values = {}
-                for i in range(10):
-                    j = str(i)
-                    if not pd.isnull(row[j]): values[j] = row[j]
-                print(values)
-                questions.append(
-                    Question(
-                        code=row['code'],
-                        text=row['text'],
-                        group=Group.objects.filter(name=row['group']).first(),
-                        values=values
-                    )
-                )
-            Question.objects.bulk_create(questions)
+        Response.objects.all().delete()
+        responses = []
+
+        # Get hash dict of question objects
+        questions = {}
+        for q in Question.objects.all():
+            questions[q.code] = q
+            
+        for dataset in os.listdir('./data/datasets'):
+            if not dataset.endswith('.csv'):
+                continue
+            with open(dataset) as f:
+                df = pd.read_csv(f, header=0)
+                year = [int(token) for token in dataset.split('.') if token.isdigit()][0]
+                # get relevant columns for this table
+                cols = [c for c in df.keys if c in questions.keys()]
+                print("{0}: columns: {1}".format(dataset, cols))
+
+                # go through the rows creating responses
+                for _, row in df.iterrows():
+                    for col in cols:
+                        if pd.isnull(row[col]):
+                            continue
+                        r = Response(
+                                year=year
+                                question=questions[col],
+                                value=row[col])
+                        r.save()
+                        demographics = [
+
+                        ]
+                        Through = Demography.response.through
+                        Through.objects.bulk_create([
+                            Through(response_id=r.pk)
+                        ])
+                
--- psppi/responses/models.py
+++ psppi/responses/models.py
@@ -2,14 +2,20 @@ from django.db import models
 from psppi.questions.models import Question
 from django.contrib.postgres.fields import HStoreField
 
+
 class Demography(models.Model):
     code = models.CharField(unique=True, max_length=128)
     text = models.CharField(default='', max_length=512)
     values = HStoreField(default={})
-    
+
+
 class Response(models.Model):
     year = models.IntegerField(default=0)
     question = models.ForeignKey(Question, null=True)
     value = models.IntegerField(default=-1)
-    demographics = models.ManyToManyField(Demography)
 
+
+class Demographic(models.Model):
+    response = models.ForeignKey(Response, null=True)
+    nature = models.ForeignKey(Demography, null=True)
+    value = models.IntegerField(default=0)
